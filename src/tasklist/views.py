e4c944dfdbe8698e847b3e1439044e2cc682d205
Aaron Madison <aaron.l.madison@gmail.com>
2010-11-07 05:55:38 -0600

    adding skeleton for existing views

--- src/tasklist/views.py
+++ src/tasklist/views.py
@@ -1,6 +1,49 @@
 # Create your views here.
 
-from django import http
+from ext import template
+from tasklist import models
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def index(request):
-    return http.HttpResponse("<h1>Hello World</h1>")
+    return template.render(request, "index.html", {
+        'tasklists': models.TaskList.get_tasklists(request.user),
+    })
+
+#def add_task(request, list_id):
+#    form = forms.AddTaskForm(request.POST or None)
+#    if form.is_valid():
+#        list = models.List.get_by_id(int(list_id))
+#        form.save(list)
+#        return http.HttpResponseRedirect(reverse('listapp:tasks', args=[list.key().id()]))
+#    return template.render(request, "add_task.html", {
+#        'form': form,
+#    })
+#
+#def add_list(request):
+#    form = forms.AddListForm(request.POST or None)
+#    if form.is_valid():
+#        form.save()
+#        return http.HttpResponseRedirect(reverse('listapp:index'))
+#    return template.render(request, "add_list.html", {
+#        'form': form,
+#    })
+#
+#def tasks(request, list_id=None):
+#    list = models.List.get_by_id(int(list_id))
+#    return template.render(request, "tasks.html", {
+#        'list': list,
+#        'tasks': models.Task.all().filter('list =', list).fetch(1000)
+#    })
+#
+#def change_status(request):
+#    logging.warn("we're at the ajax test")
+#    logging.warn("Task id: " + request.POST.get('task'))
+#    task = models.Task.get_by_id(int(request.POST.get('task')))
+#    if task.status == 'active':
+#        task.status = "completed"
+#    elif task.status == 'completed':
+#        task.status = 'active'
+#    task.put()
+#    return http.HttpResponse("success!")
